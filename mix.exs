7a3b9ff77086a233d65b4b60f80b57d73de35f2d
Gary Rennie <gazler@gmail.com>
2020-06-10 09:53:12 +0100

    release: v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UeberauthProcore.Mixfile do
   def project do
     [
       app: :ueberauth_procore,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
